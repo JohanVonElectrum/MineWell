74b7ac0b1dcd6ffa487b157f282ee034992bf4cc
JohanVonElectrum <joanestradium@gmail.com>
2020-11-11 20:04:32 +0100

    Backup2

--- src/main/java/net/minewell/engine/graphics/Renderer.java
+++ src/main/java/net/minewell/engine/graphics/Renderer.java
@@ -1,93 +1,74 @@
 package net.minewell.engine.graphics;
 
+import net.minewell.engine.components.GameObject;
+import net.minewell.engine.components.MeshRenderer;
+import net.minewell.engine.components.Transform;
 import net.minewell.engine.core.Shader;
 import net.minewell.engine.core.ShaderProgram;
 import net.minewell.engine.core.Window;
 import net.minewell.engine.exceptions.InvalidTypeException;
 import net.minewell.engine.exceptions.ShaderException;
-import org.lwjgl.system.MemoryUtil;
-
-import java.nio.FloatBuffer;
+import net.minewell.engine.math.Matrix4f;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.*;
 
 public class Renderer {
 
+    private static final float FOV = (float) Math.toRadians(60.0f);
+    private static final float Z_NEAR = 0.01f;
+    private static final float Z_FAR = 1000.f;
+    private Matrix4f projectionMatrix;
+
+
     private ShaderProgram shaderProgram;
 
-    private int vboId;
-    private int vaoId;
-
-    public void init() throws ShaderException, InvalidTypeException {
+    public void init(Window window) throws ShaderException, InvalidTypeException {
         Shader vertexShader = new Shader("/assets/shaders/vertex.glsl", GL_VERTEX_SHADER);
         Shader fragmentShader = new Shader("/assets/shaders/fragment.glsl", GL_FRAGMENT_SHADER);
         this.shaderProgram = new ShaderProgram(vertexShader, fragmentShader);
         this.shaderProgram.link();
 
-        float[] vertices = new float[]{
-                0.0f,  0.5f, 0.0f,  //center-top
-                -0.5f, -0.5f, 0.0f, //left-bottom
-                0.5f, -0.5f, 0.0f   //right-bottom
-        };
+        setProjectionMatrix((float) window.getWidth() / window.getHeight());
 
-        FloatBuffer vbo = null;
-        try {
-            vbo = MemoryUtil.memAllocFloat(vertices.length);
-            vbo.put(vertices).flip();
-
-            this.vaoId = glGenVertexArrays();
-            glBindVertexArray(this.vaoId);
-
-            this.vboId = glGenBuffers();
-            glBindBuffer(GL_ARRAY_BUFFER, this.vboId);
-            glBufferData(GL_ARRAY_BUFFER, vbo, GL_STATIC_DRAW);
-
-            glEnableVertexAttribArray(0);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-
-            glBindBuffer(GL_ARRAY_BUFFER, GL_FALSE);
-            glBindVertexArray(GL_FALSE);
-        } finally {
-            if (vbo != null)
-                MemoryUtil.memFree(vbo);
-        }
+        shaderProgram.createUniform("projectionMatrix");
+        shaderProgram.createUniform("worldMatrix");
     }
 
     public void clear() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
-    public void render(Window window) {
+    public void render(Window window, GameObject[] gameObjects) {
         clear();
 
         if (window.isResized()) {
             glViewport(0, 0, window.getWidth(), window.getHeight());
+            setProjectionMatrix((float) window.getWidth() / window.getHeight());
             window.setResized(false);
         }
 
         this.shaderProgram.bind();
+        this.shaderProgram.setUniform("projectionMatrix", this.projectionMatrix);
 
-        glBindVertexArray(this.vaoId);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        for(GameObject gameObject : gameObjects) {
+            // Set world matrix for this item
+            Matrix4f worldMatrix = ((Transform) gameObject.getComponent(Transform.class)).getWorldMatrix();
+            shaderProgram.setUniform("worldMatrix", worldMatrix);
+            // Render the mes for this game item
+            ((MeshRenderer) gameObject.getComponent(MeshRenderer.class)).render();
+        }
 
-        glBindVertexArray(GL_FALSE);
         this.shaderProgram.unbind();
     }
 
     public void dispose() {
         if (this.shaderProgram != null)
             this.shaderProgram.dispose();
+    }
 
-        glDisableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, GL_FALSE);
-        glDeleteBuffers(this.vboId);
-
-        glBindVertexArray(GL_FALSE);
-        glDeleteVertexArrays(this.vaoId);
+    protected void setProjectionMatrix(float aspectRatio) {
+        this.projectionMatrix = (Matrix4f) new Matrix4f().setPerspective(Renderer.FOV, aspectRatio, Renderer.Z_NEAR, Renderer.Z_FAR);
     }
 
 }
--- src/main/java/net/minewell/game/InGameLogic.java
+++ src/main/java/net/minewell/game/InGameLogic.java
@@ -1,35 +1,81 @@
 package net.minewell.game;
 
+import net.minewell.engine.components.Component;
+import net.minewell.engine.components.GameObject;
+import net.minewell.engine.components.MeshRenderer;
+import net.minewell.engine.components.Transform;
 import net.minewell.engine.core.IGameLogic;
 import net.minewell.engine.core.Window;
+import net.minewell.engine.exceptions.MeshException;
+import net.minewell.engine.graphics.Mesh;
 import net.minewell.engine.graphics.Renderer;
+import net.minewell.engine.math.Vector3f;
+import net.minewell.engine.utils.TimeUtils;
+import net.minewell.game.blocks.Block;
+
+import java.util.ArrayList;
+
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
 
 public class InGameLogic implements IGameLogic {
 
     Renderer renderer;
+    Mesh mesh;
+
+    private int direction = 0;
+    private float color = 0.0f;
 
     public InGameLogic() {
         this.renderer = new Renderer();
     }
 
+    GameObject go;
+
     @Override
-    public void init() throws Exception {
-        renderer.init();
+    public void init(Window window) throws Exception {
+        renderer.init(window);
+
+        this.mesh = Block.createMesh((byte) 0x15);
+
+        ArrayList<Component> gcs = new ArrayList<>();
+        gcs.add(new MeshRenderer(this.mesh));
+        this.go = new GameObject(gcs);
+
+        ((Transform) go.getComponent(Transform.class)).translate((Vector3f) Vector3f.BACKWARD.mul(5));
     }
 
     @Override
     public void input(Window window) {
-
+        if (window.isKeyPressed(GLFW_KEY_UP)) {
+            direction = 1;
+        } else if (window.isKeyPressed(GLFW_KEY_DOWN)) {
+            direction = -1;
+        } else {
+            direction = 0;
+        }
     }
 
     @Override
     public void update(double deltaTime) {
+        color += direction * 0.01f;
+        if (color > 1) {
+            color = 1.0f;
+        } else if (color < 0) {
+            color = 0.0f;
+        }
 
+        ((Transform) go.getComponent(Transform.class)).rotate(0.01f, Vector3f.UP);
+        ((Transform) go.getComponent(Transform.class)).getPosition().y = (float) Math.sin(TimeUtils.getTime());
     }
 
     @Override
     public void render(Window window) {
-        this.renderer.render(window);
+        window.setClearColor(color, color, color, 0.0f);
+
+        this.renderer.render(window, new GameObject[] {
+                go
+        });
     }
 
     @Override
